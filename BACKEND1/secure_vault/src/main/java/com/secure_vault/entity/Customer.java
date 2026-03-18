2bf2fc7d7db1abbc9def9d991b8f0dbe4be5679f
AKHIL <itsmeakhil9999@gmail.com>
2026-03-18 15:08:15 +0530

    Add User and Customer entities

---
+++ BACKEND1/secure_vault/src/main/java/com/secure_vault/entity/Customer.java
@@ -0,0 +1,62 @@
+package com.secure_vault.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String aadhaar;
+    private String bankAccount;
+
+    public Customer() {
+    }
+
+    public Customer(Long id, String name, String aadhaar, String bankAccount) {
+        this.id = id;
+        this.name = name;
+        this.aadhaar = aadhaar;
+        this.bankAccount = bankAccount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAadhaar() {
+        return aadhaar;
+    }
+
+    public void setAadhaar(String aadhaar) {
+        this.aadhaar = aadhaar;
+    }
+
+    public String getBankAccount() {
+        return bankAccount;
+    }
+
+    public void setBankAccount(String bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+}
